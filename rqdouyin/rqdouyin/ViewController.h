605ff704df76445872b14dd6f38c4e7c694e670b
sdwangwenwen <870183756@qq.com>
2018-03-23 20:21:39 +0800

    第一次提交

---
+++ rqdouyin/rqdouyin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rqdouyin
+//
+//  Created by 王文文 on 2018/3/23.
+//  Copyright © 2018年 wangwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ rqdouyin/rqdouyin/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  rqdouyin
+//
+//  Created by 王文文 on 2018/3/23.
+//  Copyright © 2018年 wangwen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
